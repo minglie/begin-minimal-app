2a9d4cf27371013fbdbf7c88bbc3373cbb07a5c2
minglie <39237517+minglie@users.noreply.github.com>
2019-11-16 16:37:18 +0800

    Update index.js

--- src/http/post-sql/index.js
+++ src/http/post-sql/index.js
@@ -2,31 +2,17 @@
 // https://docs.begin.com/en/functions/http/
 //
 // let begin = require('@architect/functions')
+var Db=require("./Db.js");
+
 
 exports.handler = async function http(req) {
-var postStr='';
-req.on('data',function(chunk){
-postStr+=chunk;
-})
-req.on('end',function(err,chunk) {
-req.body=postStr;  /*表示拿到post的值*/
-postData="";
-try {
-postData = url_module.parse("?"+req.body,true).query;
-} catch (e) {
-try {
-    postData = JSON.parse(req.body);
-}catch (e) {
-}
-}
-req.params = postData
-                              
+
                               
  return {
     headers: {
       'content-type': 'text/html; charset=utf8'
     },
-    body: JSON.stringify(req.params)
+    body: JSON.stringify(Db.doSql)
   }
   
   
