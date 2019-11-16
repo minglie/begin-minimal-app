ec0c38ddf87e2ce33572f66b064632e07d43a40b
minglie <39237517+minglie@users.noreply.github.com>
2019-11-16 15:50:27 +0800

    Update index.js

--- src/http/post-doSql/index.js
+++ src/http/post-doSql/index.js
@@ -1,12 +1,43 @@
-// Enable secure sessions, express-style middleware, and more:
-// https://docs.begin.com/en/functions/http/
-//
-// let begin = require('@architect/functions')
+var Db = require("./DbService.js");
+var M = require("ming_node");
+var url_module=require('url');
+
 
 exports.handler = async function http(req) {
-  console.log(req)
-  return {
-    status: 302,
-    location: '/'
-  }
+  var postStr='';
+  req.on('data',function(chunk){
+      postStr+=chunk;
+  })
+  req.on('end',function(err,chunk) {
+      req.body=postStr;  /*表示拿到post的值*/
+      postData="";
+      try {
+          postData = url_module.parse("?"+req.body,true).query;
+      } catch (e) {
+          try {
+              postData = JSON.parse(req.body);
+          }catch (e) {
+          }
+      }
+      req.params = postData;
+    
+     try{
+             let sql=req.params.sql
+             var rows= await Db.doSql(req.params.sql);
+             return {
+              headers: {
+                'content-type': 'text/html; charset=utf8'
+              },
+              body: JSON.stringify(rows)
+            }
+        }catch (e){
+            return {
+               headers: {
+                'content-type': 'text/html; charset=utf8'
+              },
+              body: M.result("err")
+            }
+        }
+    
+ 
 }
