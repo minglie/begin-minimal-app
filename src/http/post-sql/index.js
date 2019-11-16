95a79c090b0d1e8e57d690df2df4fb7b6f6f97a7
minglie <39237517+minglie@users.noreply.github.com>
2019-11-16 16:06:02 +0800

    Update index.js

--- src/http/post-sql/index.js
+++ src/http/post-sql/index.js
@@ -4,9 +4,30 @@
 // let begin = require('@architect/functions')
 
 exports.handler = async function http(req) {
-  console.log(req)
-  return {
-    status: 302,
-    location: '/'
+var postStr='';
+req.on('data',function(chunk){
+postStr+=chunk;
+})
+req.on('end',function(err,chunk) {
+req.body=postStr;  /*表示拿到post的值*/
+postData="";
+try {
+postData = url_module.parse("?"+req.body,true).query;
+} catch (e) {
+try {
+    postData = JSON.parse(req.body);
+}catch (e) {
+}
+}
+req.params = postData
+                              
+                              
+ return {
+    headers: {
+      'content-type': 'text/html; charset=utf8'
+    },
+    body: JSON.stringify(req.params)
   }
+  
+  
 }
