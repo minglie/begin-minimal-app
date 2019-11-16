27d93a58880bdb00ba974705e1f73ebc4ce9ec82
minglie <39237517+minglie@users.noreply.github.com>
2019-11-16 12:09:46 +0800

    Update index.js

--- src/http/get-data/index.js
+++ src/http/get-data/index.js
@@ -1,38 +1,37 @@
-// Enable secure sessions, express-style middleware, and more:
-// https://docs.begin.com/en/functions/http/
-//
-// let begin = require('@architect/functions')
 
-let html = `
-<!doctype html>
-<html lang=en>
-  <head>
-    <meta charset=utf-8>
-    <title>Hi!</title>
-    <link rel="stylesheet" href="https://static.begin.app/starter/default.css">
-    <link href="data:image/x-icon;base64,iVBORw0KGgoAAAANSUhEUgAAAAEAAAABCAQAAAC1HAwCAAAAC0lEQVR42mNkYAAAAAYAAjCB0C8AAAAASUVORK5CYII=" rel="icon" type="image/x-icon" />
-  </head>
-  <body>
-    <h1 class="center-text">
-      Hello world!
-    </h1>
-    <p class="center-text">
-      Your new route is ready to go!
-    </p>
-    <p class="center-text">
-      Learn more about building <a href="https://docs.begin.com/en/functions/http/" class="link" target="_blank">Begin HTTP functions here</a>.
-    </p>
-  </body>
-</html>
-`
+let data = require('@begin/data')
+let begin = require('@architect/functions') // Reads & writes session data
+
+var option={
+    table: 'accounts',
+    key: "content"
+}
+
+
 
 // HTTP function
 exports.handler = async function http(req) {
-  console.log(req)
+  
+  var param=req.queryStringParameters;
+  if(param && param.content){
+    
+        await data.set({...option,content:param.content})
+       return {
+          headers: {
+            'content-type': 'text/html; charset=utf8'
+          },
+          body: "ok"
+        }
+   
+  }
+  
+  
+  let greetings = await data.get({...option})
+
   return {
     headers: {
       'content-type': 'text/html; charset=utf8'
     },
-    body: html
+    body: JSON.stringify(greetings)
   }
 }
