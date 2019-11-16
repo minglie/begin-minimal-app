6ddd51be6dc01f7b18582a33d1f6d745bc76e95b
minglie <39237517+minglie@users.noreply.github.com>
2019-11-16 13:52:06 +0800

    Update index.js

--- src/http/get-demo/index.js
+++ src/http/get-demo/index.js
@@ -1,14 +1,16 @@
 
-let html = `
-hello
-`
 
 // HTTP function
 exports.handler = async function http(req) {
+  
+  var param=req.queryStringParameters;
+ 
+  
+
   return {
     headers: {
       'content-type': 'text/html; charset=utf8'
     },
-    body: html;
+    body: JSON.stringify(param)
   }
 }
