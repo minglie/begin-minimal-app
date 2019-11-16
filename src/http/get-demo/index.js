c3eddb22e276292efa9fa8bf17682d3916d78226
minglie <39237517+minglie@users.noreply.github.com>
2019-11-16 13:56:05 +0800

    Update index.js

--- src/http/get-demo/index.js
+++ src/http/get-demo/index.js
@@ -1,16 +1,16 @@
 
-
+var M=0;
 // HTTP function
 exports.handler = async function http(req) {
   
   var param=req.queryStringParameters;
  
-  
+  M=M+1;
 
   return {
     headers: {
       'content-type': 'text/html; charset=utf8'
     },
-    body: JSON.stringify(param)
+    body: M+" "+JSON.stringify(param)
   }
 }
